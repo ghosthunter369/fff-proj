75314861073578a422ac034ac79da5e1f0f737e0
ghosthunter369 <2771651667@qq.com>
2024-09-27 22:38:33 +0800

    Day02结束，写了个异常处理还有添加课程的接口，全面异常处理还是挺好，还能自定义异常没问题，但要配置，四个类自己看

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
@@ -1,8 +1,13 @@
 package com.xuecheng.content.api;
 
+import com.xuecheng.content.model.dto.AddCourseDto;
+import com.xuecheng.content.model.dto.CourseBaseInfoDto;
 import com.xuecheng.content.model.dto.CourseCategoryTreeDto;
+import com.xuecheng.content.service.CourseBaseInfoService;
 import com.xuecheng.content.service.CourseCategoryService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +17,19 @@ import java.util.List;
 public class CourseCategoryController {
     @Autowired
     private CourseCategoryService courseCategoryService;
+    @Autowired
+    private CourseBaseInfoService courseBaseInfoService;
     @RequestMapping("course-category/tree-nodes")
     public List<CourseCategoryTreeDto> queryTreeNodes() {
-        List<CourseCategoryTreeDto> list =    courseCategoryService.queryTreeNodes("1");
+        List<CourseCategoryTreeDto> list = courseCategoryService.queryTreeNodes("1");
         return list;
+    }
+
+    @ApiOperation("新增课程基础信息")
+    @RequestMapping("course")
+    public CourseBaseInfoDto createCourseBase(@RequestBody AddCourseDto addCourseDto) {
+        Long id = 1234L;
+        CourseBaseInfoDto courseBase = courseBaseInfoService.createCourseBase(id, addCourseDto);
+        return courseBase;
     }
 }
